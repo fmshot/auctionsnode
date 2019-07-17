deacf40d091a29d56ceb5f7ba8e23f595320f003
Ehirim Emeka <bluegrayonics@gmail.com>
2019-07-17 11:39:54 +0100

    setting up to push to Heroku

--- db.js
+++ db.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/AuctionsDB', (err) => {
+// mongoose.connect('mongodb://localhost:27017/AuctionsDB', (err) => {
+//     if (!err)
+//         console.log('MongoDB connection succeeded..');
+//     else
+//         console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
+// });
+
+
+mongoose.connect('mongodb+srv://fmshot:nazareneadebayo1@auctionsng-k6ges.mongodb.net/test?retryWrites=true&w=majority', (err) => {
     if (!err)
         console.log('MongoDB connection succeeded..');
     else
